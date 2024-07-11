0bc0c75055c51f8669b78be053ebb91aa2622d58
Tom Bebbington <tophattedcoder@gmail.com>
2024-07-11 17:56:11 +0100

    Show basic nav

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,22 @@ import "./style.css"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <RouterView/>
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="sticky top-0 h-20 border-b-2 border-gray-200 flex flex-row items-center px-2 text-2xl ">
+
+      <div class="font-bold">
+        PokeDex
+      </div>
+      <div class="flex-1">
+
+      </div>
+
+      <RouterLink to="/list/pokemon">
+        Pokemon
+      </RouterLink>
+    </div>
+    <div class="p-2 overflow-scroll">
+
+      <RouterView/>
+    </div>
+</template>
